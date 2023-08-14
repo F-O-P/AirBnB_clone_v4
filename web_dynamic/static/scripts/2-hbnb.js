127684f9070464c3847985bfe597984296b18f4f
sarahmarkland <sarah.markland@icloud.com>
2023-08-14 16:12:33 -0500

    task 3.1

--- web_dynamic/static/scripts/2-hbnb.js
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -1,5 +1,18 @@
 $(document).ready(function () {
   const amenityDict = {};
+
+  function updateAPIStatus () {
+    $.get('http://0.0.0.0:5001/api/v1/status/', function (data, status) {
+      if (data.status === 'OK') {
+        $('DIV#api_status').addClass('available');
+      } else {
+        $('DIV#api_status').removeClass('available');
+      }
+    });
+  }
+
+updateAPIStatus();
+
   $('input[type=checkbox]').click(function () {
     if ($(this).is(':checked')) {
       amenityDict[$(this).data('id')] = $(this).data('name');
@@ -7,6 +20,6 @@ $(document).ready(function () {
       delete amenityDict[$(this).data('id')];
     }
     $('.amenities h4').text(Object.values(amenityDict).join(', '));
-  });
-}
-);
+  }
+  );
+});
